1447d7dd469a805b461cf816d5fbbd3a9ef805d9
Nikeyshon <nico_bonnin@hotmail.com>
2017-05-24 02:58:26 -0300

    :D

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -25,10 +25,10 @@ public class Main {
         
         
         VistaLogIn vista = new VistaLogIn();
-        vista.setVisible(true);
+       // vista.setVisible(true);
 
         //Creamos tes instancias de Contacto  (int codCliente, String nombre, String direccion, String codigoPostal, String telefono, String dni, Double saldo
-        /*Cliente cliente1 = new Cliente("Pedro", "dr clark 385", "3260", "86484",15531,1.2,51.0);
+        Cliente cliente1 = new Cliente("Pedro", "dr clark 385", "3260", "86484",15531,1.2,51.0);
         Cliente cliente2 = new Cliente("Manuel", "dr clark 385", "3260", "86484",15531,1.2,51.0); 
         Cliente cliente3 = new Cliente("Martin", "dr clark 385", "3260", "86484",15531,1.2,51.0);
         Cliente cliente4 = new Cliente("Josecito", "dr clark 385", "3260", "86484",15531,1.2,51.0);
@@ -37,8 +37,8 @@ public class Main {
         Producto produc1 = new Producto("Mate", "Un mate con y sin yerba",date.getTimeDate() , 18.2, 5, 2, 4, 7, true, date.getTimeDate());
         
         genericDAO.save(produc1);
-        System.out.println("ta");*/
-        
+        System.out.println("ta");
+        vista.setVisible(true);
         //List<Cliente> listaContactos = clienteDAO.obtenListaContactos();
 
         //for(Cliente c : listaContactos)
